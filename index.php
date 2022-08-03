f7417062f1ba2153080f6551911c57176867df5e
NegritoLindo123 <cc1001447@gmail.com>
2022-08-03 15:51:07 -0500

    add index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 
         //FUNCIÓN PARA SUMAR DOS NUMEROS
-        function sumar($numero2,$numero3){
+        function sumar($numero2,$numero3,$numero4){
 
             $suma = $numero1 + $numero2;
             $operacion = "Suma";
